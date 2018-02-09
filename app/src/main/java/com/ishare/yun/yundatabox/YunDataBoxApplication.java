2d47376e7452bf2cb3733762a8480a0d0115cf32
Zhao, Yaming <yaming.zhao@sap.com>
2018-02-10 01:14:37 +0800

    Initial commit for yun data box.

---
+++ app/src/main/java/com/ishare/yun/yundatabox/YunDataBoxApplication.java
@@ -0,0 +1,10 @@
+package com.ishare.yun.yundatabox;
+
+import zuo.biao.library.base.BaseApplication;
+
+/**
+ * Created by i310736(Yaming.Zhao@sap.com) on 02/10/2018.
+ */
+
+public class YunDataBoxApplication extends BaseApplication {
+}
